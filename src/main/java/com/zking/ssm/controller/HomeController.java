4da9a1150de3b84961ee84270093a0b4fb6700a1
Administrator <2939844480@qq.com>
2018-12-26 16:34:32 +0800

    all File

--- src/main/java/com/zking/ssm/controller/HomeController.java
+++ src/main/java/com/zking/ssm/controller/HomeController.java
@@ -35,7 +35,7 @@ public class HomeController {
     @RequestMapping(value="toHomeL")
     public String toHomeL(RedirectAttributes model){
         System.out.println(model);
-        System.out.println("dsdssssssssssssssssssssssssssssssssssssssssssssssssseeeeeeeeeeeeeeesssss");
+        System.out.println("deesss");
 
         return "welcome";
     }
